4b72f1b54457f26dff713bd768c17fb5231a573e
seutio22 <seutio22@github.com>
2025-10-03 17:17:44 -0300

    fix: Forçar deploy Vercel com DataCleanup.tsx atualizado - Adicionar comentário para forçar mudança - Garantir que Vercel build a versão mais recente - Resolver erro de import DataCleanup no Vercel

--- demandas-web/src/pages/DataCleanup.tsx
+++ demandas-web/src/pages/DataCleanup.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { DataCleanup } from '../components/DataCleanup'
+// DataCleanup page component
 
 const DataCleanupPage: React.FC = () => {
   return <DataCleanup />
