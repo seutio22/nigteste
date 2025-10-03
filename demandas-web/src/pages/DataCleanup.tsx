5f4f446e7aa42f9c556cde4b392637c691f9d36e
seutio22 <seutio22@github.com>
2025-10-03 17:16:37 -0300

    trigger: Forçar novo deploy Vercel com versão mais recente - Adicionar DataCleanup.tsx que estava faltando - Garantir que Vercel build a versão mais recente - Resolver problema de build com commit antigo

---
+++ demandas-web/src/pages/DataCleanup.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { DataCleanup } from '../components/DataCleanup'
+
+const DataCleanupPage: React.FC = () => {
+  return <DataCleanup />
+}
+
+export default DataCleanupPage
