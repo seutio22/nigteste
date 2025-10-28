0fb4cbec58c0d154753486ae99de7defd85ca6c6
seutio22 <seutio22@github.com>
2025-10-28 12:08:12 -0300

    fix: Corrigir erro config undefined no SmartImporter da pagina Dados - v0.5.23

--- demandas-web/src/pages/Dados.tsx
+++ demandas-web/src/pages/Dados.tsx
@@ -15,6 +15,7 @@ import { SmartImporter } from '../components/SmartImporter'
 import { BulkDeleteModal } from '../components/BulkDeleteModal'
 import { useBulkDelete } from '../hooks/useBulkDelete'
 // CleanupModal removido - função de limpeza de duplicatas removida
+import { smartImporterConfigs } from '../config/smartImporterConfigs'
 import type { TabKey, FormData, DataMap } from '../types/dadosTypes'
 import type { ImportResult } from '../types/smartImporter'
 
@@ -1340,6 +1341,7 @@ export default function DadosPage() {
         open={smartImporterOpen}
         onClose={() => setSmartImporterOpen(false)}
         onImport={handleSmartImport}
+        config={smartImporterConfigs[activeTab] || smartImporterConfigs.clientes}
         masterData={store}
       />
 
